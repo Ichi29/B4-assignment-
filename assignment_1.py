ca1f2a458a7865ac9fb7678fb29174ed452e4ed0
鈴木一朗 <suzuki.ichiro.me@tut.jp>
2026-04-06 15:40:48 +0900

    modified:   assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -1,3 +1,16 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
+
+def transform_point(H, x, y):
+    p = np.array([x, y, 1.0])
+    q = H @ p
+    x_prime = q[0] / q[2]
+    y_prime = q[1] / q[2]
+    return x_prime, y_prime
+
+H= np.array([
+    [1, 0, 100],
+    [0, 1, 50],
+    [0,0,1]
+], dtype=float)
+
+print(transform_point(H, 10, 20))
